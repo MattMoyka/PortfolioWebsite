e43218b643a7e4b49cdbbfcc15cccd37bce21922
mattmoyka <mmoyka17@gmail.com>
2021-11-11 10:47:43 -0500

    logo added

--- client/src/layout/Layout.jsx
+++ client/src/layout/Layout.jsx
@@ -1,12 +1,13 @@
 import './layout.css'
 import { NavHashLink } from 'react-router-hash-link'
 import { Icon } from '@iconify/react';
+import personallogo from '../assets/personallogo.png'
 
 export default function Layout(props) {
   return (
     <div >
       <nav className='nav-container'>
-        <div className='nav-title'>Matt Moyka</div>
+        <img className='personal-logo' src={personallogo} alt='matt moyka software engineer' />
         <div className='nav-links'>
           <NavHashLink activeStyle={{ color: 'white' }} smooth to='/#who' id='nav-link-font'>Who Am I?</NavHashLink>
           <NavHashLink activeStyle={{ color: 'white' }} smooth to='/#projects' id='nav-link-font'>Projects</NavHashLink>
