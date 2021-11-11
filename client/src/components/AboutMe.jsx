f54bd2bb05f1bda45901c6455df27b9e697bf812
mattmoyka <mmoyka17@gmail.com>
2021-11-10 19:10:51 -0500

    spacers added

--- client/src/components/AboutMe.jsx
+++ client/src/components/AboutMe.jsx
@@ -4,11 +4,11 @@ import Landing from './Landing'
 export default function AboutMe() {
   return (
     <div>
-      <div id='who' className='blocker'></div>
+      <div id='who' className='blocker-top'></div>
       <div className='aboutme-container'>
         <div className='aboutme-text'>
           <div className='aboutme-title' >Who?</div>
-          <p className='aboutme-p'>Hi I'm Matt Moyka, a junior developer mixed with an experienced mechanical engineer. Matt is looking to break into the software devlopement role and apply his advanced problem solving skills and improve his software development knowledge.</p>
+          <p className='aboutme-p'>Hi I'm Matt Moyka, a junior developer mixed with a degree and work experience in mechanical engineer. I am looking to break into the software devlopement role and apply his advanced problem solving skills and improve his software development knowledge.</p>
         </div>
         <Landing />
       </div>
