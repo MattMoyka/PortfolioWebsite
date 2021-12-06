8a90bbfe7df4e65c627a0ad580c1d865a04163d9
mattmoyka <mmoyka17@gmail.com>
2021-12-06 12:02:22 -0500

    fresh start"

--- client/src/components/Landing.jsx
+++
@@ -1,9 +0,0 @@
-import headshot from '../assets/headshot.jpg'
-
-export default function Landing() {
-  return (
-    <div>
-      <img src={headshot} id='landing-img' height='400px' alt='guy with idea' />
-    </div>
-  )
-}
