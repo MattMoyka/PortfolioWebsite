33ef8bad5dcd5900e75c528717054970446865e6
mattmoyka <mattmoyka@gmail.com>
2022-01-17 11:45:54 -0500

    media queries on the landing page

--- client-v2/src/screens/Landing.jsx
+++ client-v2/src/screens/Landing.jsx
@@ -11,17 +11,20 @@ export default function Landing() {
   const history = useHistory()
   return (
     <Box sx={{ height: '110vh', width: '100vw', position: 'absolute', top: '-10', backgroundColor: '#e3f2fd' }}>
-      <Box sx={{ width: { xs: '60vw', sm: '100vw' }, maxWidth: 500, textAlign: 'center', margin: '100px auto', height: '45vh' }}>
-        <Typography variant='h2'  >
-          Matt Moyka
-        </Typography>
-        <Typography variant='body1' sx={{ fontSize: '1.5rem' }}>
-          I am a fullstack developer/mechanical engineer.
-        </Typography>
-        <Box sx={{ display: 'flex', flexDirection: 'column', width: { xs: '30vw', md: '20vw' }, margin: '0 auto' }}>
+      <Box sx={{ width: { xs: '60vw', sm: '100vw' }, textAlign: { xs: 'center', md: 'left' }, margin: { xs: '100px auto', md: '100px' }, height: '45vh', display: { md: 'flex' }, justifyContent: { md: 'space-around', gap: { md: '300px' } } }}>
+        <Box>
+          <Typography variant='h2'  >
+            Matt Moyka
+          </Typography>
+          <Typography variant='body1' sx={{ fontSize: '1.5rem' }}>
+            I am a fullstack developer/mechanical engineer.
+          </Typography>
           <Button color='primary' onClick={() => (history.push('/Projects'))} variant='contained' sx={{ mt: '20px' }}>
             Check out my projects
           </Button>
+        </Box>
+        <Box sx={{ display: 'flex', flexDirection: 'column', width: { xs: '30vw', md: '20vw' }, margin: '0 auto' }}>
+
           <Typography sx={{ marginTop: '50px', fontSize: '1.5rem' }}>Get in touch!</Typography>
           <Box sx={{ display: 'flex', width: { xs: '30vw', md: '20vw' }, gap: '20px', justifyContent: 'space-around' }}>
             <a href='https://github.com/MattMoyka' target='_blank' rel="noreferrer" style={{ color: 'black' }}><Button color='primary' variant='contained' sx={{ mt: '20px' }}>
